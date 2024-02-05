176b86501dc96f7a15525541ff5cb0871c02c18b
erickbrunopequenodossantos@outlook.com <erickbrunopequenodossantos@outlook.com>
2024-02-05 11:56:59 -0300

    Configuracoes Inicias da API

---
+++ src/main/java/catalogo/oferta/api/ApiCatalogoProdutosApplication.java
@@ -0,0 +1,13 @@
+package catalogo.oferta.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCatalogoProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCatalogoProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/catalogo/oferta/api/ApiCatalogoProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package catalogo.oferta.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCatalogoProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
